1a4945720271210400e7d691af829f6a3cb591f6
youseefmoemen <youseefmoemen293@gmail.com>
2021-12-19 00:18:03 +0200

    almost done:)

--- src/AgatScheduler.java
+++ src/AgatScheduler.java
@@ -3,14 +3,23 @@ import java.util.ArrayList;
 public class AgatScheduler {
     double last_arrival_time;
     double max_remaining_burst_time;
-    ArrayList<ProcessAgat> processes;
+    double current_Time;
+    ArrayList<ProcessAgat> processes; //all the processes at the ready queue
     double v1;
     double v2;
     AgatScheduler(ArrayList<ProcessAgat> p){
         processes = p;
-    }
+        current_Time = 0;
 
-    void update_statics(){
+    }
+    Boolean check(){
+        for(ProcessAgat ag : processes){
+            if(ag.burstTime > 0)
+                return true;
+        }
+        return false;
+    }
+    void update_Vs(){
         last_arrival_time = processes.get(processes.size()-1).arrivalTime;
         max_remaining_burst_time = -1;
         for(ProcessAgat pg: processes){
@@ -29,14 +38,58 @@ public class AgatScheduler {
             v2 = 1;
         }
     }
-
+    void update_AgFactor(){
+        for(ProcessAgat pr: processes){
+            pr.update_agate(this);
+        }
+    }
+    int better(int index){
+        double hold = 1e9;
+        int idx = -1;
+        for(int i = 0; i < processes.size();i++){
+            if(processes.get(i).agatFactor < hold){
+                hold = processes.get(i).agatFactor;
+                idx = i;
+            }
+        }
+        if(idx == index)
+            return -1; //No one is better in the agatFactor
+        return idx;
+    }
     public void agatS(){
-        update_statics();
         int index = 0;
-        //TODO the break condition
-        while(true){
-            index = (index + 1) % processes.size();
-
+        while(check()){
+            if(processes.get(index).quantumFactor >= processes.get(index).burstTime){ //0.4Q > buresetTime => No need to work
+                current_Time += processes.get(index).burstTime;
+                processes.remove(index); //delete the process
+                continue;
+            }
+            current_Time += processes.get(index).quantumFactor; //update the currentTime
+            processes.get(index).burstTime -= processes.get(index).quantumFactor; //update the burset time
+            update_Vs();
+            update_AgFactor();
+            int otherIndex = better(index);
+            if(otherIndex != -1){
+                if(processes.get(index).burstTime > (processes.get(index).quantum - processes.get(index).quantumFactor)){
+                    processes.get(index).burstTime -= (processes.get(index).quantum - processes.get(index).quantumFactor);
+                    current_Time += (processes.get(index).quantum - processes.get(index).quantumFactor);
+                    processes.get(index).quantum += 2;
+                    ProcessAgat holder = processes.get(index);
+                    processes.remove(index);
+                    processes.add(holder);
+                    continue;
+                }else if(processes.get(index).burstTime <= (processes.get(index).quantum - processes.get(index).quantumFactor)){
+                    current_Time += processes.get(index).burstTime;
+                    processes.remove(index);
+                }
+            }else{
+                //some one is better than you;
+                //TODO update the quantum here
+                ProcessAgat holder = processes.get(index);
+                processes.set(index, processes.get(otherIndex));
+                processes.add(holder);
+                continue;
+            }
         }
     }
 }
--- src/ProcessAgat.java
+++ src/ProcessAgat.java
@@ -2,14 +2,16 @@ import java.awt.Color;
 public class ProcessAgat extends Process{
     int quantum;
     double agatFactor;
-
+    double quantumFactor; //quantum * 0.4
     
     ProcessAgat(String name, Color color, int arrivalTime, int burstTime, int priorityNumber, int q) {
         super(name, color, arrivalTime, burstTime, priorityNumber);
         quantum = q;
+        quantumFactor = Math.round(q * 0.4);
     }
 
     void update_agate(AgatScheduler agatScheduler){
+        //all of them share the same V1, V2
         agatFactor = (10 - priorityNumber) + Math.ceil(arrivalTime / agatScheduler.v1) + Math.ceil(burstTime / agatScheduler.v2);
     }
 
